27e382352d8bc5132981355516a746295eb035ad
heisen04 <anshuman01.tripathi@gmail.com>
2017-10-29 11:49:24 +0530

    define modules

--- simple linear regression/simple_linear_regression.py
+++ simple linear regression/simple_linear_regression.py
@@ -9,23 +9,34 @@ Created on Sat Oct 28 22:38:13 2017
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-data = pd.read_csv('Salary_Data.csv') #LOAD DATASET
-X = data.iloc[:,:-1].values #GET THE DEPENDENT VARIABLE SET
-Y = data.iloc[:,-1].values #GET THE INDEPENDENT VARIABLE SET (to be predicted)
-
 from sklearn.cross_validation import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 1/3, random_state = 0)
-
 from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor.fit(x_train, y_train)
-y_pred = regressor.predict(x_test)
 
+def data_preparation(file_name):
+    global X, Y, x_test, x_train, y_test, y_train
+    data = pd.read_csv('Salary_Data.csv') #LOAD DATASET
+    X = data.iloc[:,:-1].values #GET THE DEPENDENT VARIABLE SET
+    Y = data.iloc[:,-1].values #GET THE INDEPENDENT VARIABLE SET (to be predicted)
+    #DIVIDE THE DATA INTO TEST AND TRAINING SETS
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 1/3, random_state = 0)
 
-plt.scatter(X, Y, color='red')
-plt.plot(x_test, regressor.predict(x_test), color='Blue')
-plt.title("Salary vs Experience")
-plt.ylabel("Salary")
-plt.xlabel("Experience")
-plt.plot
+def create_regressor():
+    global regressor
+    regressor = LinearRegression()
+    regressor.fit(x_train, y_train)
+    
+def predict_data():
+    y_pred = regressor.predict(x_test)
+    print(y_pred)
+
+def plot_chart():
+    plt.scatter(X, Y, color='red')
+    plt.plot(x_test, regressor.predict(x_test), color='Blue')
+    plt.title("Salary vs Experience")
+    plt.ylabel("Salary")
+    plt.xlabel("Experience")
+    plt.plot
+    
+data_preparation('Salary_Data.csv')
+create_regressor()
+plot_chart()
