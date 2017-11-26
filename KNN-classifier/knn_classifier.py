c7828576db1314071d05768692836b9fe6b80b36
heisen04 <anshuman01.tripathi@gmail.com>
2017-11-26 19:54:14 +0530

    KNN classifier code

---
+++ KNN-classifier/knn_classifier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 26 19:51:51 2017
+
+@author: heisenberg
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_dataset():
+    global dataset, X, Y
+    dataset = pd.read_csv('')
