4ebce86c063cce0f00590b1f9e39ca2b0f0b50be
heisen04 <anshuman01.tripathi@gmail.com>
2017-11-26 16:59:26 +0530

    added logistic regression code and dataset

--- logistic_regression(classification)/logistic_regressor.py
+++ logistic_regression(classification)/logistic_regressor.py
@@ -6,3 +6,10 @@ Created on Sun Nov 26 16:55:29 2017
 @author: heisenberg
 """
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_dataset():
+    global dataset, X, Y
+    dataset = pd.read_csv('Social_Network_Ads.csv')
