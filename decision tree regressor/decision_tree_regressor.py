755f9601e4b3838d1d1d3db5640c868298d44528
heisen04 <anshuman01.tripathi@gmail.com>
2017-11-26 14:07:31 +0530

    random forest regressor code and dataset

--- decision tree regressor/decision_tree_regressor.py
+++ decision tree regressor/decision_tree_regressor.py
@@ -6,3 +6,36 @@ Created on Sun Nov 26 12:49:32 2017
 @author: heisenberg
 """
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_dataset():
+    global dataset, X, Y
+    dataset = pd.read_csv('Position_Salaries.csv')
+    X = dataset.iloc[:, 1:2].values
+    Y = dataset.iloc[:, 2:3].values
+
+#SCALLING NOT NEEDED AS THE DECISION TREE LIBRARY AUTOMATICALLY DOES THAT
+    
+def create_train_regressor():
+    global regressor
+    from sklearn.tree import DecisionTreeRegressor
+    regressor = DecisionTreeRegressor()
+    regressor.fit(X,Y)
+    
+def predict_values(value):
+    pred = regressor.predict(value)
+    print(pred)
+
+def plot_graph():
+    plt.scatter(X,Y, color='blue')
+    x_grid = np.arange(min(X), max(X), 0.01)
+    x_grid = x_grid.reshape(len(x_grid),1)
+    plt.plot(x_grid, regressor.predict(x_grid), color = 'red')
+    plt.show()
+
+load_dataset()
+create_train_regressor()
+predict_values(6.5)
+plot_graph()
---
+++ random forest regression/random_forest_regressor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 26 14:06:19 2017
+
+@author: heisenberg
+"""
+
