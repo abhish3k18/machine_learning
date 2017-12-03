6c6dbe121d2525f2472eaa03bd44423dbb4f78ba
heisen04 <anshuman01.tripathi@gmail.com>
2017-12-03 10:24:14 +0530

    added rbf kernel in svm

--- RBF kernel SVM/classifier_code.py
+++ RBF kernel SVM/classifier_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Dec  2 13:40:11 2017
+Created on Sun Dec  3 10:07:02 2017
 
 @author: heisenberg
 """
@@ -12,40 +12,39 @@ import matplotlib.pyplot as plt
 
 def load_dataset():
     global dataset, X, Y
-    dataset = pd.read_csv("Social_Network_Ads.csv")
+    dataset = pd.read_csv('Social_Network_Ads.csv')
     X = dataset.iloc[:,2:4].values
     Y = dataset.iloc[:,-1].values
-    
+
 def scale_data():
     global X
     from sklearn.preprocessing import StandardScaler
     sc_x = StandardScaler()
-    sc_x.fit(X)
-    X=sc_x.transform(X)
-    
-def split_train_test():
-    global x_train, x_test, y_test, y_train
+    X = sc_x.fit_transform(X)
+
+def split_data():
+    global x_train, x_test, y_train, y_test 
     from sklearn.model_selection import train_test_split
-    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+    x_train, x_test, y_train, y_test  = train_test_split(X, Y, test_size=0.2)
     
 def create_train_model():
     global classifier
     from sklearn.svm import SVC
-    classifier = SVC(kernel = 'linear')
-    classifier.fit(x_train,y_train)
+    classifier = SVC(kernel='rbf')
+    classifier.fit(x_train, y_train)
     
 def predict_values():
     global y_pred
     y_pred = classifier.predict(x_test)
     
-def analyse_confusion_matrix():
+def analysis():
     from sklearn.metrics import confusion_matrix
-    cm = confusion_matrix(y_test, y_pred)
+    cm = confusion_matrix(y_test,y_pred)
     print(cm)
+
     
 def plot_graph():
     from matplotlib.colors import ListedColormap
-    global X1, X2
     x_set, y_set = x_train, y_train
     X1, X2 = np.meshgrid(np.arange(start = min(x_set[:,0])-1, stop = max(x_set[:,1])+1, step = 0.01),
                          np.arange(start = min(x_set[:,1])-1, stop = max(x_set[:,1])+1, step = 0.01))
@@ -56,15 +55,15 @@ def plot_graph():
     for i,j in enumerate(np.unique(y_set)):
         plt.scatter(x_set[y_set==j,0], x_set[y_set==j,1], 
                     c=ListedColormap(('red','green'))(i), label=j)
-    plt.title("SVM Classification with RBF kernel")
+    plt.title("SVM Classification")
     plt.xlabel("Age")
     plt.ylabel("Salary")
-    plt.legend()
+    plt.legend()    
     
 load_dataset()
 scale_data()
-split_train_test()
+split_data()
 create_train_model()
 predict_values()
-analyse_confusion_matrix()
+analysis()
 plot_graph()
